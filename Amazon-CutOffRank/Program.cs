bab64e82443c33fba6f786b3e05a6430f292a86c
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-09-20 22:19:39 -0700

    Add project files.

---
+++ Amazon-CutOffRank/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Amazon_CutOffRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(cutOffRank(3, new int[] { 100, 75,50, 50, 25 }));
+        }
+
+        static int cutOffRank(int cutOff, int[] scores)
+        {
+
+            int[] freq = new int[100001];
+            int max = int.MinValue;
+            foreach(int score in scores)
+            {
+                freq[score]++;
+                if (max < score) 
+                    max = score;
+            }
+
+            int i = max, res = 0, rank = 1;
+            while (i >= 0 && rank <= cutOff)
+            {
+                if (freq[i] != 0)
+                {
+                    res += freq[i];
+                    rank += freq[i];
+                }
+                i--;
+            }
+            return res;
+        }
+    }
+}
